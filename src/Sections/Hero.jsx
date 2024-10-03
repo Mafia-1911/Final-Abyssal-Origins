be187fc05687ebad37d972498e70fea97dcaa0b3
Mushtasin Ahmed <mushtasin.ahmed11@gmail.com>
2024-10-04 02:11:53 +0600

    Add Game Link

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const Hero = () => {
+  const redirectToGame = () => {
+    window.location.href = 'https://mafia-1911.github.io/test-game/'; // Replace with the desired URL
+  }
   return (
     <section class="hero-section">
     <div class="hero-overlay"></div>
     <div class="hero-content">
       <h1>Explore the Depths of the Sea</h1>
       <p>Discover the wonders of the ocean, learn about marine life, and explore underwater ecosystems.</p>
-      <a href="#learn-more" class="btn btn-primary">Learn More</a>
+      <a href="#learn-more" class="btn btn-primary" onClick={redirectToGame}>PLAY THE GAME</a>
     </div>
   </section>
   )
